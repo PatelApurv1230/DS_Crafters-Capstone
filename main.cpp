94367b7f60fc84c5cda10500d8256f8b5ac3f86b
Farang998 <202301209@daiict.ac.in>
2024-03-12 18:47:53 +0530

    file reading

--- main.cpp
+++ main.cpp
@@ -1,31 +1,41 @@
 #include <iostream>
+#include <bits/stdc++.h>
 #include <filesystem>
+#include <fstream>
 #include <string>
-#include <cctype>
-#include <unordered_map>
 using namespace std;
+
+namespace file_s=filesystem;
+
+
 int main()
 {
+    string path_;
+    int first_Ndocuments;
 
-    string temp;
-    unordered_map <string,int>m1;
-    for(int i=0;i<10;i++)
+    cout<<"Enter the total number of initial documents: ";
+    cin>>first_Ndocuments;
+
+    int count_=0;
+    string path_;
+    while(count_!=first_Ndocuments)
     {
-        getchar();
-        cin>>temp;
-        for (char &c : temp) {
-        c = tolower(c);
-        }
-        if(m1.count(temp)==0)
-        {
-            m1.insert(pair<string,int>(temp,1));
-        }
-        else{
-            m1[temp]++;
-        }
+        cout<<"Enter the path of folder: "<<"\n";
+        cin>>path_;
+    for(const auto& entry :file_s::directory_iterator("path_"))
+    {
+        ifstream initialDocuments(entry.path());
+        count_++;
+
     }
-    for (auto it = m1.begin(); it != m1.end(); it++) {
-    cout << it->first << ": " << it->second << endl;
     }
+
+
     return 0;
 }
+
+
+
+
+
+
